f057ef37efce5dc98e324257707b1ebfe58c6576
fabiotano <fabio.capriotti20@gmail.com>
2023-11-23 10:07:07 -0300

    moved Home component in page.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import './globals.css';
 import Header from '../components/Header';
-import Home from '../components/Home';
 import Footer from '@/components/Footer';
 
 export default function RootLayout({ children }) {
@@ -8,9 +7,6 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <body>
                 <Header />
-                <Home />
-                {/* {children} */}
-                {/* <footer>este es el footer</footer> */}
                 {children}
                 <Footer />
             </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Home from "../components/Home";
 
 function page() {
-    return <div>page</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default page;

44088e270b089700db21fa28476ee198f33567f4
Leo Turzi <leogturzi@gmail.com>
2023-11-28 02:31:06 -0700

    Add different carousels to main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,31 @@
+import BrandCarousel from '@/components/BrandCarousel';
+import Carousel from '@/components/Carousel';
+import ProductCarousel from '@/components/ProductCarousel';
+
 import React from 'react';
 
 function page() {
-    return <div>page</div>;
+  return (
+    <div className="container m-auto ">
+      <Carousel />
+      <section>
+        <h2 className="text-2xl font-bold text-center my-2">
+          Productos Destacados
+        </h2>
+        <p className="text-sm text-center text-gray-400">
+          Tienda Online de productos de Peluqueria y Estetica
+        </p>
+        <ProductCarousel />
+      </section>
+      <section>
+        <h2 className="text-2xl font-bold text-center my-2">Nuestras Marcas</h2>
+        <p className="text-sm text-center text-gray-400">
+          Tienda Online de productos de Peluqueria y Estetica
+        </p>
+        <BrandCarousel />
+      </section>
+    </div>
+  );
 }
 
 export default page;

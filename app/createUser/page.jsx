588ab91a6ac06b2d6f69b91274ed6a46feda2589
fabiotano <fabio.capriotti20@gmail.com>
2023-12-15 16:47:52 -0300

    added media queries to the forms

--- app/createUser/page.jsx
+++ app/createUser/page.jsx
@@ -23,93 +23,93 @@ export default function CreateUser() {
       <section className="my-6">
         <form className="w-3/4 m-auto max-w-[600px]">
 
-          <div className="pt-6 pb-4 md:flex">
+          <div className="pt-4 md:pt-6 pb-4 md:pb-6 md:flex">
             <label
               id="name"
-              className="text-sm w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
+              className="text-sm md:text-base w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
             >
-              Trattamento
+              Sesso
             </label>
             <select
               id="tratamiento"
-              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs outline-none"
+              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs md:text-sm outline-none"
             >
               <option value="" disabled selected>
-                Seleziona un trattamento
+                Seleziona un sesso
               </option>
-              <option value="sgn">Sgn</option>
-              <option value="sgra">Sgra</option>
+              <option value="sgn">Maschile</option>
+              <option value="sgra">Femminile</option>
             </select>
           </div>
 
-          <div className="pb-4 md:flex">
+          <div className="pb-4 md:pb-6 md:flex">
             <label
               id="name"
-              className="text-sm w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
+              className="text-sm md:text-base w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
             >
               Nome
             </label>
             <input
               name="name"
-              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs outline-none"
+              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs md:text-sm outline-none"
               type="text"
             />
           </div>
 
-          <div className="pb-4 md:flex">
+          <div className="pb-4 md:pb-6 md:flex">
             <label
               id="surname"
-              className="text-sm w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
+              className="text-sm md:text-base w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
             >
               Cognome
             </label>
             <input
               name="surname"
-              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs outline-none"
+              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs md:text-sm outline-none"
               type="text"
             />
           </div>
 
-          <div className="pb-4 md:flex">
+          <div className="pb-4 md:pb-6 md:flex">
             <label
               id="birthdate"
-              className="text-sm w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
+              className="text-sm md:text-base w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
             >
               Data di nascita
             </label>
             <input
               name="birthdate"
-              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs outline-none"
+              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs md:text-sm outline-none"
               type="date"
             />
           </div>
 
-          <div className="pb-4 md:flex">
+          <div className="pb-4 md:pb-6 md:flex">
             <label
               id="email"
-              className="text-sm w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
+              className="text-sm md:text-base w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
             >
               Indirizzo email
             </label>
             <input
               name="email"
-              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs outline-none"
+              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs md:text-sm outline-none"
               type="text"
               placeholder="Inserisci la tua email"
             />
           </div>
 
-          <div className="pb-12 md:flex">
+          <div className="pb-8 md:pb-12 md:flex">
             <label
               id="password"
-              className="text-sm w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
+              className="text-sm md:text-base w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
             >
               Password
             </label>
             <div className="relative w-full">
               <input
                 name="password"
-                className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs outline-none"
+                className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs md:text-sm outline-none"
                 type={showPassword ? "text" : "password"}
                 placeholder="Inserisci la tua password"
               />
@@ -124,26 +124,26 @@ export default function CreateUser() {
           <div className="md:w-[444px] ml-auto">
             <button
               type="button"
-              className="bg-blue-500 text-white w-full h-11 rounded-sm text-xs hover:bg-blue-500"
+              className="bg-blue-500 text-white w-full h-11 rounded-sm text-xs md:text-sm hover:bg-blue-500"
             >
               Crea con GOOGLE
             </button>
           </div>
 
-          <div className="py-4 md:w-[444px] ml-auto">
+          <div className="py-4 md:py-6 md:w-[444px] ml-auto">
             <button
               type="button"
-              className="bg-black text-white w-full h-11 rounded-sm text-xs hover:bg-blue-500"
+              className="bg-black text-white w-full h-11 rounded-sm text-xs md:text-sm hover:bg-blue-500"
             >
               Crea account
             </button>
           </div>
 
-          <hr className="border-t border-grey-200 m-4"></hr>
+          <hr className="border-t border-grey-200 my-2"></hr>
 
-          <div className="md:w-[444px] ml-auto border-t-1 border-black">
+          <div className="md:w-[444px] ml-auto border-t-1 py-2 md:py-4 border-black">
             <Link href="/login">
-              <p className="text-gray-800 text-sm text-center hover:underline">
+              <p className="text-gray-800 text-sm md:text-base text-center hover:underline hover:text-blue-500">
                 Hai già un account? Accedi qui
               </p>
             </Link>
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -21,32 +21,33 @@ export default function Login() {
       </div>
       <section className="my-6">
         <form className="w-3/4 m-auto max-w-[600px]">
-          <div className="pt-6 pb-4 md:flex">
+
+          <div className="pt-6 pb-4 md:pt-9 md:pb-7 md:flex">
             <label
               id="email"
-              className="text-sm w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
+              className="text-sm md:text-base w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
             >
               Indirizzo email
             </label>
             <input
               name="email"
-              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs outline-none"
+              className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs md:text-sm outline-none"
               type="text"
               placeholder="Inserisci la tua email"
             />
           </div>
 
-          <div className="pb-7 md:flex">
+          <div className="pb-6 md:pb-9 md:flex">
             <label
               id="password"
-              className="text-sm w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
+              className="text-sm md:text-base w-[140px] font-bold py-2 shrink-0 block md:mr-4 md:text-right"
             >
               Password
             </label>
             <div className="relative w-full">
               <input
                 name="password"
-                className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs outline-none"
+                className="bg-gray-100 border border-gray-200 w-full h-10 rounded-sm p-2 text-xs md:text-sm outline-none"
                 type={showPassword ? "text" : "password"}
                 placeholder="Inserisci la tua password"
               />
@@ -60,26 +61,26 @@ export default function Login() {
 
           <div className="md:w-[444px] ml-auto">
             <Link href="/forgot-password">
-              <p className="text-gray-500 text-sm text-center hover:underline">
+              <p className="text-gray-500 text-sm md:text-base text-center hover:underline hover:text-blue-500">
                 Hai dimenticato la password?
               </p>
             </Link>
           </div>
 
-          <div className="py-5 md:w-[444px] ml-auto">
+          <div className="py-5 md:py-8 md:w-[444px] ml-auto">
             <button
               type="button"
-              className="bg-black text-white w-full h-11 rounded-sm text-xs hover:bg-blue-500"
+              className="bg-black text-white w-full h-11 rounded-sm text-xs md:text-sm hover:bg-blue-500"
             >
               Accedi
             </button>
           </div>
 
-          <hr className="border-t border-grey-200 m-4"></hr>
+          <hr className="border-t border-grey-200 my-2"></hr>
 
-          <div className="md:w-[444px] ml-auto border-t-1 border-black">
+          <div className="md:w-[444px] ml-auto border-t-1 py-3 md:py-6 border-black">
             <Link href="/createUser">
-              <p className="text-gray-800 text-sm text-center hover:underline">
+              <p className="text-gray-800 text-sm md:text-base text-center hover:underline hover:text-blue-500">
                 Non hai un account? Crea uno qui
               </p>
             </Link>

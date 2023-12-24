7c87c53eceb169e7fb16dcc6289b7de5574f5c8f
fabiotano <fabio.capriotti20@gmail.com>
2023-12-24 17:08:32 -0300

    style when hovering over regiones

--- components/MapChart.jsx
+++ components/MapChart.jsx
@@ -1,20 +1,45 @@
 'use client'
-import React from "react"
-import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+import styles from "./MapChart.module.css";
+import React, { useState } from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 const geoUrl =
-  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
+  "https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_regions.geojson";
 
 export default function MapChart() {
+  // Estado para almacenar la región que se está haciendo hover
+  const [hoveredRegion, setHoveredRegion] = useState(null);
+
   return (
-    <ComposableMap>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-    </ComposableMap>
-  )
+    <div className={styles.mapContainer}>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                style={{
+                  default: {
+                    fill: "#808080",
+                    outline: "none",
+                  },
+                  hover: {
+                    fill: "blue",
+                    outline: "none",
+                  },
+                  pressed: {
+                    fill: "yellow",
+                    outline: "none",
+                  },
+                }}
+              />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
+    </div>
+  );
 }
+
+

cd598c44354e2f924c1a361e270ea6ab3b112100
Grow <fabio@MacBook-Air-de-Valentina.local>
2024-04-19 16:41:52 -0400

    fix some style on footer and map

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -23,7 +23,7 @@ function Home() {
         <BrandCarousel />
       </section>
 
-      <section className="pt-8 px-8 pb-6 sm:pb-8 ">
+      <section className="pt-8 px-8 pb-8 sm:pb-8 ">
         <h2 className="text-2xl font-bold text-center">Categorie</h2>
         <p className="text-sm text-center text-gray-400 mt-2 mb-4">
           Scopri la nostra vasta gamma di prodotti per capelli, accessori e
--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,107 +1,96 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <footer className="border">
-            <div className="max-width">
-                <div className="md:flex-row  sm:flex-col sm:flex bg-red-500 sm:py-4">
-                    <div className="text-gray-50 flex align-top px-4 pt-4">
-                        <Image
-                            className="align-top"
-                            src={'/favoritos.svg'}
-                            width={32}
-                            height={32}
-                        />
-                        <div className="ps-2">
-                            <h3 className="font-bold text-lg m-2">
-                                Lista dei favoriti
-                            </h3>
-                            <p className="text-sm">
-                            Guarda nel tuo profilo gli articoli che preferisci, in modo da poterli tenere d'occhio fino a quando arriverà il momento di acquistarli.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="text-gray-50 flex align-top px-4 py-4">
-                        <Image
-                            src={'/entrega_rapida.svg'}
-                            width={32}
-                            height={32}
-                        />
-                        <div className="ps-2">
-                            <h3 className="font-bold text-lg m-2">
-                                Consegna veloce
-                            </h3>
-                            <p className="text-sm">
-                            Tutte le spedizioni vengono effettuate nel minor tempo possibile, garantito. Durante l'elaborazione dell'ordine, ti forniremo una data di consegna garantita o stimata.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="text-gray-50 flex align-top px-4 py-4">
-                        <Image src={'/24_7.svg'} width={32} height={32} />
-                        <div className="ps-2">
-                            <h3 className="font-bold text-lg m-2">Ordini disponibili 24/7</h3>
-                            <p className="text-sm">
-                            Gestiamo i tuoi ordini in qualsiasi momento del giorno e della settimana, cercando sempre di offrirti il miglior servizio possibile.
-
-
-
-
-
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className="border">
+      <div className="max-width">
+        <div className="md:flex-row  sm:flex-col sm:flex bg-red-500 sm:py-4">
+          <div className="text-gray-50 flex align-top px-4 pt-4">
+            <Image
+              className="align-top"
+              src={"/favoritos.svg"}
+              width={32}
+              height={32}
+            />
+            <div className="ps-2">
+              <h3 className="font-bold text-lg m-2">Lista dei favoriti</h3>
+              <p className="text-sm">
+                Guarda nel tuo profilo gli articoli che preferisci, in modo da
+                poterli tenere d'occhio fino a quando arriverà il momento di
+                acquistarli.
+              </p>
             </div>
-            <div className="flex p-6 max-width px-8 container ">
-                <div className="flex flex-col">
-                    <Image src={'/logoShe.png'} height={70} width={100} />
-                    <p className="text-gray-500 text-sm w-64 mt-2">
-                        Skin Hair Expo SRL
-                    </p>
-                    <p className="text-gray-500 text-sm mt-2">
-                       Tortoreto 64018 (TE) 
-                    </p>
-                </div>
-                <div className="flex flex-col xs:hidden ps-4">
-                    <Link href={''} className="text-gray-500 font-bold mb-4">
-                        Productos
-                    </Link>
-                    <Link href={''} className="text-gray-500 text-sm pb-1">
-                        Peluqueria
-                    </Link>
-                    <Link href={''} className="text-gray-500 text-sm pb-1">
-                        Estetica
-                    </Link>
-                    <Link href={''} className="text-gray-500 text-sm pb-1">
-                        Marcas
-                    </Link>
-                </div>
-                <div className="flex flex-col xs:hidden ps-4">
-                    <Link href={''} className="text-gray-500 font-bold mb-4">
-                        Informacion
-                    </Link>
-                    <Link href={''} className="text-gray-500 text-sm pb-1">
-                        Contacto
-                    </Link>
-                    <Link href={''} className="text-gray-500 text-sm pb-1">
-                        Sobre Nosotros
-                    </Link>
-                    <Link href={''} className="text-gray-500 text-sm pb-1">
-                        FAQ
-                    </Link>
-                </div>
+          </div>
+          <div className="text-gray-50 flex align-top px-4 py-4">
+            <Image src={"/entrega_rapida.svg"} width={32} height={32} />
+            <div className="ps-2">
+              <h3 className="font-bold text-lg m-2">Consegna veloce</h3>
+              <p className="text-sm">
+                Tutte le spedizioni vengono effettuate nel minor tempo
+                possibile, garantito. Durante l'elaborazione dell'ordine, ti
+                forniremo una data di consegna garantita o stimata.
+              </p>
             </div>
-            <div className="h-6 bg-gray-200 flex justify-center align-middle">
-                <p className="text-center text-gray-500 text-xs m-auto">
-                    &copy;2023 - SHE. All rights reserved.
-                </p>
+          </div>
+          <div className="text-gray-50 flex align-top px-4 py-4">
+            <Image src={"/24_7.svg"} width={32} height={32} />
+            <div className="ps-2">
+              <h3 className="font-bold text-lg m-2">Ordini disponibili 24/7</h3>
+              <p className="text-sm">
+                Gestiamo i tuoi ordini in qualsiasi momento del giorno e della
+                settimana, cercando sempre di offrirti il miglior servizio
+                possibile.
+              </p>
             </div>
-        </footer>
-    );
+          </div>
+        </div>
+      </div>
+      <div className="flex p-6 max-width px-8 container items-end">
+        <div className="flex flex-col">
+          <Image src={"/logoShe.png"} height={80} width={120} />
+        </div>
+        <div className="flex flex-col xs:hidden ps-6">
+          <p className="text-gray-500 text-sm w-64">Skin Hair Expo SRL</p>
+          <Link href="" className="text-gray-500 text-sm mt-2">
+            Tortoreto 64018 (TE)
+          </Link>
+        </div>
+        <div className="flex flex-col xs:hidden ps-1">
+          <Link href={""} className="text-gray-500 text-sm font-bold mb-1">
+            Prodotti
+          </Link>
+          <Link href={""} className="text-gray-500 text-xs pb-1">
+            Peluqueria
+          </Link>
+          <Link href={""} className="text-gray-500 text-xs">
+            Marche
+          </Link>
+        </div>
+        <div className="flex flex-col xs:hidden ps-6">
+          <Link href={""} className="text-gray-500 text-sm font-bold mb-1">
+            Informazioni
+          </Link>
+          <Link
+            href={"https://wa.me/393294131136"}
+            className="text-gray-500 text-xs pb-1"
+          >
+            Contatti
+          </Link>
+          <Link href={"/us"} className="text-gray-500 text-xs">
+            About us
+          </Link>
+        </div>
+      </div>
+      <div className="h-6 bg-gray-200 flex justify-center align-middle">
+        <p className="text-center text-gray-500 text-xs m-auto">
+          &copy;2023 - SHE. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;

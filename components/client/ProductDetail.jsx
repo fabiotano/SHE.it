d3c46345a7084b68a4af3c85d9657da9e80e6cc7
fabiotano <fabio.capriotti20@gmail.com>
2024-05-21 20:18:31 +0200

    productDetail laptop version

--- components/client/ProductDetail.jsx
+++ components/client/ProductDetail.jsx
@@ -2,21 +2,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faHeart } from "@fortawesome/free-solid-svg-icons";
 import ReactImageMagnify from "react-image-magnify";
-
 import Link from "next/link";
 
 function ProductCard(props) {
   const { product } = props;
   return (
     <div className="container">
+      {/* Breadcrumb Navigation */}
       <div className="text-2xs py-2 pl-3">
         <p>
-          <Link href={"/"}>Inicio</Link> /{" "}
+          <Link href="/">Inicio</Link> /{" "}
           <span className="text-gray-300">Parrucchieria/ Producto</span>
         </p>
       </div>
 
-      <div className="flex items-center justify-center block sm:hidden">
+      {/* Logo for mobile view */}
+      <div className="flex items-center justify-center md:hidden">
         <img
           src={product.brandLogo}
           className="object-contain h-16 sm:h-18"
@@ -24,35 +25,37 @@ function ProductCard(props) {
         />
       </div>
 
-      <div className="md:flex md:gap-8 w-full max-w-[350px] md:max-w-none mx-auto md:justify-evenly my-16">
-  <div className="relative flex-shrink-0 justify-center border items-center md:w-[300px] h-[280px] md:h-[350px] ">
-    <div className="flex items-center justify-center h-full">
-          <div className="object-cover ">
-            <ReactImageMagnify
-              {...{
-                smallImage: {
-                  alt: 'Wristwatch by Ted Baker London',
-                  isFluidWidth: true,
-                  src: product.image
-              },
-              largeImage: {
-                  src: product.image,
-                  width: 1200,
-                  height: 1800
-              },          
+      {/* Main content area */}
+      <div className="md:grid grid-cols-1 md:grid-cols-2 md:gap-8 w-full md:max-w-none mx-auto md:justify-evenly mt-1 mb-4 md:mt-10 md:mb-6">
+        {/* Product image with magnify feature */}
+        <div className="relative max-w-[350px] m-auto flex-shrink-0 justify-center mt-28 items-center border md:w-[350px] md:h-[350px]">
+          <div className="flex items-center justify-center h-full">
+            <div className="object-cover">
+              <ReactImageMagnify
+                {...{
+                  smallImage: {
+                    alt: "Wristwatch by Ted Baker London",
+                    isFluidWidth: true,
+                    src: product.image,
+                  },
+                  largeImage: {
+                    src: product.image,
+                    width: 1200,
+                    height: 1800,
+                  },
                   isHintEnabled: true,
-                  enlargedImagePosition: 'over',
-                  lensStyle: {backgroundColor: 'rgba(0,0,0,0.6)'}
-              }}
-            />
+                  enlargedImagePosition: "over",
+                  lensStyle: { backgroundColor: "rgba(0,0,0,0.6)" },
+                }}
+              />
+            </div>
           </div>
         </div>
-        </div>
 
-
-        <div className="w-full md:w-[500px] relative flex flex-col justify-between">
+        {/* Product details */}
+        <div className="w-full m-auto relative flex flex-col justify-between">
           <div>
-            <h3 className="text-center font-semibold text-md md:text-2xl mt-3">
+            <h3 className="text-center font-semibold text-md md:text-xl mt-3">
               {product.title}
             </h3>
             <h3 className="text-center text-sm md:text-xl mt-1">
@@ -64,18 +67,26 @@ function ProductCard(props) {
             <h6 className="text-center text-xs md:text-md text-gray-400">
               {/* {product.desc} */}
             </h6>
-            <h4 className="text-center font-bold text-md md:text-lg mt-3">
+            <h4 className="text-center font-bold text-xl mt-3">
               &euro;
               {` ${Math.floor(product.price)}`}
               <sup>{(product.price % 1).toFixed(2).substring(2)}</sup>
             </h4>
-            <h3 className="text-justify bg-red-100 break-all mt-4 px-6 py-2">
-              TTTESTTTESTTESTTTTESTTTESTTESTTTTESTTTESTTEST
-              TTTESTTTESTTESTTTTESTTTESTTESTTTTESTTTESTTEST
-              TTTESTTTESTTESTTTTESTTTESTTESTTTTESTTTESTTEST
-              TTTESTTTESTTESTTTTESTTTESTTESTTTTESTTTESTTEST
-            </h3>
           </div>
+
+          {/* Product description */}
+          <div>
+            <p className="max-w-[600px] m-auto text-justify bg-gray-50 break-all mt-4 px-8 py-4 leading-8">
+              Disfruta de una experiencia de sonido superior con los auriculares
+              inalámbricos Bluetooth Pro. Con tecnología de cancelación de ruido
+              activa, estos auriculares te permiten sumergirte en tu música sin
+              distracciones. Su diseño ergonómico y ligero asegura comodidad
+              durante horas de uso. Su diseño ergonómico y ligero asegura comodidad
+              durante horas de uso.
+            </p>
+          </div>
+
+          {/* Favorite icon and brand logo for larger screens */}
           <div>
             <FontAwesomeIcon
               icon={faHeart}
@@ -83,41 +94,46 @@ function ProductCard(props) {
                 product.favorite ? "text-red-400" : "text-gray-400"
               }`}
             />
-            <div className="md: absolute md: top-0 md:left-0 md:z-50 flex items-center justify-center md:block hidden">
+            <div className="md:absolute md:top-0 md:left-0 md:z-50 items-center justify-center md:block hidden">
               <img
                 src={product.brandLogo}
                 className="object-contain h-10 md:h-12"
                 alt="Logo"
               />
             </div>
-            <form action="" className="flex px-1 mt-5 md:m-5 gap-4">
-              <label
-                htmlFor="quantity"
-                className="block text-sm font-medium text-gray-900 dark:text-gray-400 mr-1"
-              >
-                <select
-                  id="quantity"
-                  className="border border-gray-300 text-gray-900 text-sm rounded-sm block w-16 h-full text-start"
-                  defaultValue="1" // Establecer el valor predeterminado aquí
-                >
-                  <option disabled>Qnt.</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                </select>
-              </label>
-              <button
-                type="submit"
-                className="bg-black text-white w-full h-11 rounded-sm text-sm md:text-sm hover:bg-blue-400"
-              >
-                <FontAwesomeIcon icon={faCartShopping} className="mx-2" />
-                Agregar al carrito
-              </button>
-            </form>
           </div>
         </div>
       </div>
+
+      {/* Add to cart form */}
+      <form
+        action=""
+        className=" max-w-[600px] m-auto flex px-8 md:px-2 my-4 md:my-8 md:mr-6 gap-4 md:max-w-[550px] md:m-auto"
+      >
+        <label
+          htmlFor="quantity"
+          className="block text-sm font-medium text-gray-900 dark:text-gray-400 mr-1"
+        >
+          <select
+            id="quantity"
+            className="border border-gray-300 text-gray-900 rounded-md text-sm block w-16 h-full text-start"
+            defaultValue="1"
+          >
+            <option disabled>Qnt.</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+        </label>
+        <button
+          type="submit"
+          className="bg-black text-white w-full h-11 rounded-md text-sm md:text-sm hover:bg-blue-400"
+        >
+          <FontAwesomeIcon icon={faCartShopping} className="mx-2" />
+          Agregar al carrito
+        </button>
+      </form>
     </div>
   );
 }
